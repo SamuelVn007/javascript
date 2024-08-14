a01845016bb19b003572121b4e7d97ebd5bedb86
SamuelVn007 <134400857+SamuelVn007@users.noreply.github.com>
2024-08-14 10:32:03 -0300

    Add files via upload

---
+++ variaveis.js
@@ -0,0 +1,60 @@
+// Variáveis
+// tipo Number
+
+const meuNumero = 6;
+const numeroUm = 7;
+const numeroDois = 2;
+
+const soma = 3+6
+
+console.log(soma)
+
+const subtacao = numeroUm - numeroDois;
+console.log(subtacao)
+
+const multiplicacao = numeroDois * numeroUm;
+console.log(multiplicacao);
+
+const divisao = numeroDois / numeroUm;
+console.log(divisao)
+
+//Float ( Número Decimal )
+
+const numeroTres = 3.3;
+const numeroQuatro = .5 
+
+const operacaoM = numeroTres * numeroQuatro;
+console.log(operacaoM)
+
+//NaN not a number
+
+const operacaoN = "a" * meuNumero;
+console.log(operacaoN)
+
+const novaOperacao = "a" + meuNumero;
+console.log (novaOperacao)
+
+// String Name( texto )
+
+const meuNome = "Samuel"
+const frase = "Meu Nome é:"
+const issoNaoEUmNumero = "50"
+
+const minhaFrase = frase + meuNome;
+console.log(minhaFrase);
+
+const operacaoO = issoNaoEUmNumero + meuNumero;
+console.log(operacaoO);
+
+// Boolean
+// True -> verdadeiro
+// False -> falso
+
+const comparacao = 6 === meuNumero;
+console.log(comparacao)
+
+const comparacaoII = "6" === meuNumero;
+console.log(comparacaoII)
+
+const comparacaoIII = "6" == meuNumero;
+console.log(comparacaoIII)
